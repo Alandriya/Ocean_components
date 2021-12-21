4e04364c616b370b737c17440ca14ebd6735a60a
Alandriya <ashcherbinina@mail.ru>
2021-12-21 17:15:44 +0300

    4D interactive plots

--- video.py
+++ video.py
@@ -14,6 +14,7 @@ from data_processing import dataframes_to_grids
 from matplotlib.ticker import LinearLocator, FormatStrFormatter
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import ticker
+import plotly.express as px
 
 
 def truncate_colormap(cmap, minval=0.0, maxval=1.0, n=100):
@@ -44,41 +45,39 @@ def init_directory(files_path_prefix, flux_type):
     return
 
 
-def draw_4D():
-    # # plot 4D into 3D: time as Z (and only means and sigmas)
-    # means_array = np.array(means_timelist)
-    # print(means_array.shape)
-    #
-    # fig = plt.figure(figsize=(components_amount * 10, 20))
-    # fig.suptitle(f'{flux_type}\n {date.strftime("%Y-%m-%d")}', fontsize=30)
-    #
-    # for comp in range(components_amount):
-    #     X = range(0, 161)
-    #     Y = range(0, 181)
-    #     Z = np.array(range(0, timesteps))
-    #     X, Y = np.meshgrid(X, Y)
-    #     Z = np.outer(Z.T, Z)
-    #
-    #     # means
-    #     ax = fig.add_subplot(2, components_amount, comp + 1, projection='3d')
-    #     ax.set_xlim(0, 161)
-    #     ax.set_ylim(0, 181)
-    #     ax.set_zlim(0, timesteps)
-    #
-    #     color_dimension = means_array
-    #     minn, maxx = color_dimension.min(), color_dimension.max()
-    #     norm = matplotlib.colors.Normalize(minn, maxx)
-    #     m = plt.cm.ScalarMappable(norm=norm, cmap='Blues')
-    #     m.set_array([])
-    #     fcolors = m.to_rgba(color_dimension)
-    #
-    #     surf = ax.plot_surface(X, Y, Z,  facecolors=fcolors, vmin=minn, vmax=maxx, shade=False)
-    #     ax.set_title(f'Mean {comp + 1}', fontsize=20)
-    #     fig.colorbar(surf, shrink=0.5, aspect=10)
-    #
-    #     fig.tight_layout()
-    #     fig.savefig(files_path_prefix + f'videos/{flux_type}/3D/TIME_{t + 1:03d}.png')
-    #     plt.close(fig)
+def draw_4D(files_path_prefix,
+            flux_type,
+            component,
+            timesteps):
+    """
+
+    :param files_path_prefix:
+    :param component:
+    :param timesteps:
+    :return:
+    """
+    X_grid = np.arange(0, 161)
+    Y_grid = np.arange(0, 181)
+    Z_grid = np.arange(0, timesteps)
+    X, Y, Z = np.meshgrid(X_grid, Y_grid, Z_grid)
+    colors = np.zeros_like(X,dtype=float)
+    for t in range(timesteps):
+        means_grid = np.load(files_path_prefix + f'tmp_arrays/{flux_type}/means_{t}.npy')
+        colors[:, :, t] = means_grid[component].T
+
+    df = pd.DataFrame()
+    df['x'] = X.flatten()
+    df['y'] = Y.flatten()
+    df['time'] = Z.flatten()
+    df['color'] = colors.flatten()
+    df.fillna(0, inplace=True)
+    fig = px.scatter_3d(df, x='x', y='y', z='time', color='color', opacity=0.1, size_max=1,
+                        color_continuous_scale=px.colors.sequential.Blues)
+
+    # tight layout
+    fig.update_layout(margin=dict(l=0, r=0, b=0, t=0))
+    fig.update_layout(scene_aspectmode='manual', scene_aspectratio=dict(x=1, y=1, z=0.01))
+    fig.show()
     return
 
 
@@ -88,7 +87,7 @@ def draw_3D(files_path_prefix,
             means_grid,
             sigmas_grid,
             weights_grid,
-            timesteps,
+            t,
             borders):
     """
     Draws timesteps amount of .png pictures, showing 3 * components amount graphics with 3D surface plot of each
@@ -100,70 +99,69 @@ def draw_3D(files_path_prefix,
     if ocean tile, and NaN, if land tile.
     :param sigmas_grid: same as means_grid, but for sigmas
     :param weights_grid: same as weights_grid, but for weights
-    :param timesteps: amount of timesteps to draw, t goes from 0 to timesteps
+    :param t: time step counting from 0 - used in picture name
     :param borders: 3 lists of lists with borders for colorbar: global max and min for parameters for each component
     shape of each is n_component * 2
     :return:
     """
-    for t in range(timesteps):
-        # plot surface
-        date = datetime.datetime(1979, 1, 1, 0, 0) + datetime.timedelta(hours=6*(62396 - 7320) + t*24*7)
-        X = range(0, 161)
-        Y = range(0, 181)
-        X, Y = np.meshgrid(X, Y)
-        fig = plt.figure(figsize=(components_amount * 10, 30))
-        fig.suptitle(f'{flux_type}\n {date.strftime("%Y-%m-%d")}', fontsize=30)
+    # plot surface
+    date = datetime.datetime(1979, 1, 1, 0, 0) + datetime.timedelta(hours=6*(62396 - 7320) + t*24*7)
+    X = range(0, 161)
+    Y = range(0, 181)
+    X, Y = np.meshgrid(X, Y)
+    fig = plt.figure(figsize=(components_amount * 10, 30))
+    fig.suptitle(f'{flux_type}\n {date.strftime("%Y-%m-%d")}', fontsize=30)
 
-        # means
-        for comp in range(components_amount):
-            ax = fig.add_subplot(3, components_amount, comp + 1, projection='3d')
-            ax.set_xlim(0, 161)
-            ax.set_ylim(0, 181)
-            cmap = matplotlib.cm.get_cmap("Blues").copy()
-            cmap = truncate_colormap(cmap, 0.2, 1.0)
-            cmap.set_bad('white', 0.0)
+    # means
+    for comp in range(components_amount):
+        ax = fig.add_subplot(3, components_amount, comp + 1, projection='3d')
+        ax.set_xlim(0, 161)
+        ax.set_ylim(0, 181)
+        cmap = matplotlib.cm.get_cmap("Blues").copy()
+        cmap = truncate_colormap(cmap, 0.2, 1.0)
+        cmap.set_bad('white', 0.0)
 
-            masked_grid = np.ma.array(means_grid[comp], mask=np.isnan(means_grid[comp]))
-            surf = ax.plot_surface(X, Y, np.transpose(masked_grid), rstride=1, cstride=1, cmap=cmap,
-                                   linewidth=0, antialiased=False, alpha=0.3)
-            ax.set_zlim(borders[0][0], borders[0][1])
-            ax.set_title(f'Mean {comp + 1}', fontsize=20)
-            fig.colorbar(surf, shrink=0.5, aspect=10)
+        masked_grid = np.ma.array(means_grid[comp], mask=np.isnan(means_grid[comp]))
+        surf = ax.plot_surface(X, Y, np.transpose(masked_grid), rstride=1, cstride=1, cmap=cmap,
+                               linewidth=0, antialiased=False, alpha=0.3)
+        ax.set_zlim(borders[0][0], borders[0][1])
+        ax.set_title(f'Mean {comp + 1}', fontsize=20)
+        fig.colorbar(surf, shrink=0.5, aspect=10)
 
-        # sigmas
-        for comp in range(components_amount):
-            ax = fig.add_subplot(3, components_amount, components_amount + comp + 1, projection='3d')
-            ax.set_xlim(0, 161)
-            ax.set_ylim(0, 181)
-            cmap = matplotlib.cm.get_cmap("YlOrRd").copy()
-            cmap = truncate_colormap(cmap, 0.2, 1.0)
-            cmap.set_bad('white', 0.0)
+    # sigmas
+    for comp in range(components_amount):
+        ax = fig.add_subplot(3, components_amount, components_amount + comp + 1, projection='3d')
+        ax.set_xlim(0, 161)
+        ax.set_ylim(0, 181)
+        cmap = matplotlib.cm.get_cmap("YlOrRd").copy()
+        cmap = truncate_colormap(cmap, 0.2, 1.0)
+        cmap.set_bad('white', 0.0)
 
-            masked_grid = np.ma.array(sigmas_grid[comp], mask=np.isnan(sigmas_grid[comp]))
-            surf = ax.plot_surface(X, Y, np.transpose(masked_grid), rstride=1, cstride=1, cmap=cmap,
-                                   linewidth=0, antialiased=False, alpha=0.3)
-            ax.set_zlim(borders[1][0], borders[1][1])
-            ax.set_title(f'Sigma {comp + 1}', fontsize=20)
-            fig.colorbar(surf, shrink=0.5, aspect=10)
+        masked_grid = np.ma.array(sigmas_grid[comp], mask=np.isnan(sigmas_grid[comp]))
+        surf = ax.plot_surface(X, Y, np.transpose(masked_grid), rstride=1, cstride=1, cmap=cmap,
+                               linewidth=0, antialiased=False, alpha=0.3)
+        ax.set_zlim(borders[1][0], borders[1][1])
+        ax.set_title(f'Sigma {comp + 1}', fontsize=20)
+        fig.colorbar(surf, shrink=0.5, aspect=10)
 
-        # weights
-        for comp in range(components_amount):
-            ax = fig.add_subplot(3, components_amount, components_amount * 2 + comp + 1, projection='3d')
-            ax.set_xlim(0, 161)
-            ax.set_ylim(0, 181)
-            cmap = matplotlib.cm.get_cmap("jet").copy()
-            cmap.set_bad('white', 0.0)
+    # weights
+    for comp in range(components_amount):
+        ax = fig.add_subplot(3, components_amount, components_amount * 2 + comp + 1, projection='3d')
+        ax.set_xlim(0, 161)
+        ax.set_ylim(0, 181)
+        cmap = matplotlib.cm.get_cmap("jet").copy()
+        cmap.set_bad('white', 0.0)
 
-            masked_grid = np.ma.array(weights_grid[comp], mask=np.isnan(weights_grid[comp]))
-            surf = ax.plot_surface(X, Y, np.transpose(masked_grid), rstride=1, cstride=1, cmap=cmap,
-                                   linewidth=0, antialiased=False, alpha=0.3)
-            ax.set_zlim(borders[2][0], borders[2][1])
-            ax.set_title(f'Weight {comp + 1}', fontsize=20)
-            fig.colorbar(surf, shrink=0.5, aspect=10)
+        masked_grid = np.ma.array(weights_grid[comp], mask=np.isnan(weights_grid[comp]))
+        surf = ax.plot_surface(X, Y, np.transpose(masked_grid), rstride=1, cstride=1, cmap=cmap,
+                               linewidth=0, antialiased=False, alpha=0.3)
+        ax.set_zlim(borders[2][0], borders[2][1])
+        ax.set_title(f'Weight {comp + 1}', fontsize=20)
+        fig.colorbar(surf, shrink=0.5, aspect=10)
 
-        fig.tight_layout()
-        fig.savefig(files_path_prefix + f'videos/{flux_type}/3D/{t + 1:03d}.png')
-        plt.close(fig)
+    fig.tight_layout()
+    fig.savefig(files_path_prefix + f'videos/{flux_type}/3D/{t + 1:03d}.png')
+    plt.close(fig)
     return
 
 
