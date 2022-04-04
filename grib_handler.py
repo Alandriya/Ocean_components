b2ecc1037ef7e20ea77567d1a26ccde0c15f7718
Alandriya <ashcherbinina@mail.ru>
2022-04-04 16:06:39 +0300

    grib_handler v2.0

--- grib_handler.py
+++ grib_handler.py
@@ -1,8 +1,10 @@
 # import cfgrib
+import tqdm
 import xarray as xr
 import numpy as np
 import datetime
 from data_processing import load_prepare_fluxes
+from struct import unpack
 
 files_path_prefix = 'D://Data/OceanFull/'
 data_postfix = '2021'
@@ -12,24 +14,37 @@ ds = xr.open_dataset(files_path_prefix + f'{data_postfix}.grib', engine='cfgrib'
 # print(ds)
 # print(ds.coords['longitude'])
 
-# latent
-shape = ds.variables['mslhf'].shape
-latent = np.array(ds.variables['mslhf']).reshape((shape[0]*shape[1], shape[2], shape[3]))
-latent = latent[:, ::2, ::2]
-print(latent.shape)  # [timesteps, 161, 181]
-latent = latent.reshape((161 * 181, latent.shape[0]))
-np.save(files_path_prefix + f'LATENT_{data_postfix}.npy', latent)
+maskfile = open(files_path_prefix + "mask", "rb")
+binary_values = maskfile.read(29141)
+maskfile.close()
+mask = unpack('?' * 29141, binary_values)
+mask = np.array(mask, dtype=int)
+
+shape = ds.variables['msshf'].shape
 
 # sensible
-shape = ds.variables['msshf'].shape
-sensible = np.array(ds.variables['msshf']).reshape((shape[0]*shape[1], shape[2], shape[3]))
-sensible = sensible[:, ::2, ::2]
-print(sensible.shape)  # [timesteps, 161, 181]
-sensible = np.transpose(sensible)
-sensible = sensible.reshape((161*181, sensible.shape[2]))
-print(sensible.shape)  # [161, 181, timesteps]
-# sensible = sensible.reshape((161 * 181, sensible.shape[2]))
+tmp = ds.variables['msshf'].values
+sensible = np.zeros((161*181, shape[0] * shape[1]))
+for t in tqdm.tqdm(range(shape[0])):
+    for t1 in range(2):
+        for i in range(161):
+            for j in range(181):
+                sensible[i*181 + j, t*2 + t1] = tmp[t, t1, i * 2, j * 2]
+
+sensible[np.logical_not(mask), :] = np.nan
 np.save(files_path_prefix + f'SENSIBLE_{data_postfix}.npy', sensible)
+
+# latent
+tmp = ds.variables['mslhf'].values
+latent = np.zeros((161*181, shape[0] * shape[1]))
+for t in tqdm.tqdm(range(shape[0])):
+    for t1 in range(2):
+        for i in range(161):
+            for j in range(181):
+                latent[i*181 + j, t*2 + t1] = tmp[t, t1, i * 2, j * 2]
+
+latent[np.logical_not(mask), :] = np.nan
+np.save(files_path_prefix + f'LATENT_{data_postfix}.npy', latent)
 
 # group by day
 sensible_array, latent_array = load_prepare_fluxes(f'SENSIBLE_{data_postfix}.npy',
@@ -44,10 +59,10 @@ np.save(files_path_prefix + f'latent_grouped_{data_postfix}.npy', latent_array)
 # # sens_old = np.load(files_path_prefix + 'sensible_grouped_2019-2021.npy')
 # # sens_new = np.load(files_path_prefix + 'sensible_grouped_2021.npy')
 # sens_old = np.load(files_path_prefix + 'SENSIBLE_2019-2021.npy')
-# sens_new = np.load(files_path_prefix + 'SENSIBLE_2021(jul-dec).npy')
+# sens_new = np.load(files_path_prefix + 'SENSIBLE_2021.npy')
 # days_delta1 = (datetime.datetime(2021, 7, 1, 0, 0) - datetime.datetime(2019, 1, 1, 0, 0)).days * 4
 # print(days_delta1)
 # print(sens_old.shape)
 # print(sens_new.shape)
-# print(sens_old[39:41, days_delta1:days_delta1+15])
-# print(sens_new[39:41, :15])
+# print(sens_old[39:40, days_delta1:days_delta1+15])
+# print(sens_new[39:40, :15])
