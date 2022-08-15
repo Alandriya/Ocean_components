0bd167810a311e946cc69a7777675332beaefb12
Alandriya <ashcherbinina@mail.ru>
2022-08-15 07:56:56 +0300

    count_Bel_Kor_difference rewritten

--- EM_count_coefficients.py
+++ EM_count_coefficients.py
@@ -7,6 +7,7 @@ import datetime
 import tqdm
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 from video import get_continuous_cmap
+import matplotlib.dates as mdates
 
 
 def count_abf_Kor_from_points(files_path_prefix: str, time_start: int, time_end: int, point_start: int, point_end: int):
@@ -44,16 +45,38 @@ def count_abf_Kor_from_points(files_path_prefix: str, time_start: int, time_end:
     return
 
 
-def count_Bel_Kor_difference(files_path_prefix, time_start: int, time_end: int, point_start: int, point_end: int):
-    for t in tqdm.tqdm(range(time_start, time_end)):
-        for p in range(point_start, point_end):
-            if os.path.exists(files_path_prefix + f'Components/sensible/{t}_A_sens.npy'):
-                a_sens_Bel = np.load(files_path_prefix + f'Coeff_data/{t}_A_sens.npy')
-                a_sens_Kor = np.load(files_path_prefix + f'Components/sensible/{t}_A_sens.npy')
-                a_diff = a_sens_Bel - a_sens_Kor
-                a_diff[a_sens_Kor == 0] = 0
-                a_diff[np.isnan(a_sens_Bel)] = np.nan
-                np.save(files_path_prefix + f'Components/difference/{t}_A_sens.npy', a_diff)
+def count_Bel_Kor_difference(files_path_prefix, time_start: int, time_end: int, point: tuple, radius: int,
+                             n_components:int, window_width:int):
+    point_bigger = [(point[0] + i, point[1] + j) for i in range(-radius, radius + 1) for j in
+                    range(-radius, radius + 1)]
+
+    a_sens_Bel = np.zeros(time_end-time_start)
+    point_size = (radius * 2 + 1) ** 2
+
+    if not os.path.exists(files_path_prefix + f'Components/sensible/Bel/point_({point[0]}, {point[1]}).npy'):
+        for t in tqdm.tqdm(range(time_start, time_end)):
+            a_arr = np.load(files_path_prefix + f'Coeff_data/{t}_A_sens.npy')
+            a_sens_Bel[t-time_start] = sum([a_arr[p[0], p[1]] for p in point_bigger]) / point_size
+            del a_arr
+
+        a_sens_Bel = np.diff(a_sens_Bel)
+        np.save(files_path_prefix + f'Components/sensible/Bel/point_({point[0]}, {point[1]}).npy', a_sens_Bel)
+    else:
+        a_sens_Bel = np.load(files_path_prefix + f'Components/sensible/Bel/point_({point[0]}, {point[1]}).npy')
+
+    if not os.path.exists(files_path_prefix + f'Components/sensible/Sum/point_({point[0]}, {point[1]}).npy'):
+        a_sens_Kor = pd.read_excel(files_path_prefix + f'Components/sensible/point_({point[0]}, {point[1]}).xlsx')
+        a_sens_Kor.fillna(0, inplace=True)
+        a_sum = np.zeros(len(a_sens_Kor))
+        for i in range(1, n_components+1):
+            a_sum += a_sens_Kor[f'mean_{i}'] * a_sens_Kor[f'weight_{i}']
+
+        np.save(files_path_prefix + f'Components/sensible/Sum/point_({point[0]}, {point[1]}).npy', a_sum)
+    else:
+        a_sum = np.load(files_path_prefix + f'Components/sensible/Sum/point_({point[0]}, {point[1]}).npy')
+
+    a_diff = a_sens_Bel[:len(a_sum)] - a_sum
+    np.save(files_path_prefix + f'Components/sensible/difference/point_({point[0]}, {point[1]}).npy', a_diff)
     return
 
 
@@ -158,79 +181,26 @@ def plot_a_diff(files_path_prefix, time_start, time_end, start_pic_num):
     return
 
 
-def plot_difference_1d(files_path_prefix, time_start: int, time_end: int, point_start: int, point_end: int,
-                       n_components, window_width):
-    a_sens_list = list()
-    b_list = list()
-    for t in range(time_start + window_width - 7, time_end):
-        a_sens = np.load(files_path_prefix + f'Coeff_data/{t}_A_sens.npy')
-        a_sens_list.append(a_sens)
+def plot_difference_1d(files_path_prefix, time_start: int, time_end: int, point: tuple, window_width:int, radius:int):
+    a_diff = np.load(files_path_prefix + f'Components/sensible/difference/point_({point[0]}, {point[1]}).npy')
+    a_sens_Bel = np.load(files_path_prefix + f'Components/sensible/Bel/point_({point[0]}, {point[1]}).npy')
+    a_sum = np.load(files_path_prefix + f'Components/sensible/Sum/point_({point[0]}, {point[1]}).npy')
+    a_sum /= window_width
 
-        b_matrix = np.load(files_path_prefix + f'Coeff_data/{t}_B.npy')
-        b_list.append(b_matrix)
-
-    means_cols = [f'mean_{i}' for i in range(1, n_components + 1)]
-    sigmas_cols = [f'sigma_{i}' for i in range(1, n_components + 1)]
-    weights_cols = [f'weight_{i}' for i in range(1, n_components + 1)]
-
-    for p in tqdm.tqdm(range(point_start, point_end)):
-        if os.path.exists(files_path_prefix + f'Components/sensible/raw/point_{p}.xlsx'):
-            df = pd.read_excel(files_path_prefix + f'Components/sensible/raw/point_{p}.xlsx')
-            df.fillna(0, inplace=True)
-            df.columns = ['time', 'ts'] + means_cols + sigmas_cols + weights_cols
-
-            a_sens_Kor = 0.0
-            tmp_1 = 0.0
-            for comp in range(1, n_components + 1):
-                a_sens_Kor += df[f'mean_{comp}'] * df[f'weight_{comp}']
-                tmp_1 += df[f'weight_{comp}'] * (df[f'mean_{comp}'] ** 2 + df[f'sigma_{comp}']**2)
-
-            b_sens_Kor = tmp_1 - a_sens_Kor**2
-            b_sens_Kor = np.sqrt(np.array(b_sens_Kor))
-
-            a_sens_Bel = list()
-            b_sens_Bel = list()
-            for i in range(len(a_sens_list)):
-                a_sens_Bel.append(a_sens_list[i][p // 181, p % 181])
-                b_sens_Bel.append(b_list[i][0, p // 181, p % 181])
-
-            # diff = np.abs(np.array(sens_Kor_ts[time_start:time_end - window_width//2] - sens_Bel_ts))
-            # np.save(files_path_prefix + f'Components/difference_1d/point_{p}.npy', diff)
-
-            fig = plt.figure(figsize=(30, 10))
-            fig.suptitle(f'Point {p}')
-            x = range(time_start, time_end - window_width + 7)
-            colors = [0, 'Lime', 'Orchid', 'Purple', 'g', 'b']
-            # for comp in range(1, n_components + 1):
-            #     plt.plot(x,
-            #              np.array(df[f'mean_{comp}'] * df[f'weight_{comp}'])[time_start:time_end- window_width//2],
-            #              c=colors[comp], label=f'Comp_{comp}')
-            plt.plot(x, a_sens_Kor.loc[x], c='y', label='Kor')
-            # plt.plot(x, diff, c='r', label='difference')
-            # plt.legend()
-            # plt.savefig(files_path_prefix + f'Components/tmp/point_{p}_A.png')
-
-            plt.plot(x, np.array(a_sens_Bel) / window_width, c='b', label='Bel / window_width')
-            # plt.plot(x, diff, c='r', label='difference')
-            plt.legend()
-            plt.savefig(files_path_prefix + f'Components/tmp/point_{p}_A_difference.png')
-
-
-            # fig = plt.figure(figsize=(30, 10))
-            # fig.suptitle(f'Point {p}')
-            # x = range(time_start, time_end - window_width // 2)
-            # plt.plot(x, b_sens_Kor[x], c='y', label='Kor')
-            # colors = [0, 'Lime', 'Orchid', 'Purple']
-            # for comp in range(1, n_components + 1):
-            #     plt.plot(x,
-            #              np.array(df[f'weight_{comp}'] * (df[f'sigma_{comp}'] - a_sens_Kor) ** 2)[time_start:time_end- window_width//2],
-            #              c=colors[comp], label=f'Comp_{comp}')
-            # plt.legend()
-            # plt.savefig(files_path_prefix + f'Components/tmp/point_{p}_B.png')
-
-            # plt.plot(x, np.array(b_sens_Bel), c='b', label='Bel')
-            # plt.legend()
-            # plt.savefig(files_path_prefix + f'Components/tmp/point_{p}_B_difference.png')
-
-
+    date_start = datetime.datetime(1979, 1, 1, 0, 0) + datetime.timedelta(days=time_start)
+    date_end = datetime.datetime(1979, 1, 1, 0, 0) + datetime.timedelta(days=time_end)
+    fig, axs = plt.subplots(1, 1, figsize=(20, 10))
+    fig.suptitle(f'A coeff sensible at point ({point[0]}, {point[1]}) \n radius = {radius}, window = {window_width} days'
+                 f'\n {date_start.strftime("%Y-%m-%d")} - {date_end.strftime("%Y-%m-%d")}', fontsize=20, fontweight='bold')
+    axs.xaxis.set_minor_locator(mdates.MonthLocator())
+    axs.xaxis.set_major_formatter(mdates.ConciseDateFormatter(axs.xaxis.get_major_locator()))
+    x = range(0, time_end - time_start)
+    days = [datetime.datetime(1979, 1, 1) + datetime.timedelta(days=t) for t in range(time_start, time_end)]
+    # dates = None
+    axs.plot(days, a_sens_Bel[:len(x)], c='b', label='Bel')
+    axs.plot(days, a_sum[:len(x)], c='r', label='Kor')
+    # axs.plot(x, a_diff[:len(x)], c='y', label='difference')
+    axs.legend()
+    fig.tight_layout()
+    fig.savefig(files_path_prefix + f'Components/plots/sensible/difference_point_({point[0]}, {point[1]}).png')
     return
