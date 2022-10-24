22a187d5d0ce82359f5fef62042ddbfeed4e51c2
Alandriya <ashcherbinina@mail.ru>
2022-10-24 15:44:55 +0300

    parsing 2022 grib

--- grib_handler.py
+++ grib_handler.py
@@ -3,9 +3,10 @@ import tqdm
 import xarray as xr
 import numpy as np
 from struct import unpack
+import datetime
 
 files_path_prefix = 'D://Data/OceanFull/'
-data_postfix = '2019-extended'
+data_postfix = '2022-part'
 
 maskfile = open(files_path_prefix + "mask", "rb")
 binary_values = maskfile.read(29141)
@@ -14,8 +15,10 @@ mask = unpack('?' * 29141, binary_values)
 mask = np.array(mask, dtype=int)
 
 # load grib
-ds = xr.open_dataset(files_path_prefix + f'{data_postfix}.grib', engine='cfgrib', drop_variables='mslhf', chunks=-1,
-                     inline_array=True)
+# ds = xr.open_dataset(files_path_prefix + f'{data_postfix}.grib', engine='cfgrib', drop_variables='mslhf', chunks=-1,
+#                      inline_array=True)
+ds = xr.load_dataset(files_path_prefix + f'{data_postfix}.grib', engine='cfgrib')
+
 print(ds)
 print(ds.coords['valid_time'])
 print(ds.coords['valid_time'].shape)
@@ -23,23 +26,47 @@ print(ds.coords['valid_time'].shape)
 # sensible
 shape = ds.variables['msshf'].shape
 print(shape)
-tmp = ds.variables['msshf']
+# tmp = ds.variables['msshf']
+tmp = ds.variables['mslhf'] #TODO latent
 tmp = tmp[:, :, ::2, ::2]  # part of map with size 161x181
 print(tmp.shape)
 
-print(tmp[370, :, 39, 39].values)  # ok
-print(tmp[400, :, 39, 39])  # OSError
-raise ValueError
 
 
-sensible = np.zeros((161*181, shape[0] * shape[1]))
-print(sensible.shape)
-for t in tqdm.tqdm(range(0, 370)):
+# print(tmp[370, :, 39, 39].values)  # ok
+# print(tmp[400, :, 39, 39])  # OSError
+# Если не упал до сих пор, то уже успех, но не полный
+# raise ValueError
+
+
+# sensible = np.zeros((161*181, shape[0] * shape[1]))
+latent = np.zeros((161*181, shape[0] * shape[1]))
+# print(sensible.shape)
+for t in tqdm.tqdm(range(0, tmp.shape[0])):
     for i in range(161):
-        sensible[i * 181:(i + 1) * 181, t * shape[1]:(t+1)*shape[1]] = tmp[t, :, i, :].transpose().values
+        # падает на следующей строчке при индексе >= 386, если не упадет, то надо раскомментить нормальный цикл с 0
+        # sensible[i * 181:(i + 1) * 181, t * shape[1]:(t+1)*shape[1]] = tmp[t, :, i, :].transpose().values
+        latent[i * 181:(i + 1) * 181, t * shape[1]:(t + 1) * shape[1]] = tmp[t, :, i, :].transpose().values
 
-sensible[np.logical_not(mask), :] = np.nan
-np.save(files_path_prefix + f'SENSIBLE_{data_postfix}.npy', sensible)
+# sensible[np.logical_not(mask), :] = np.nan
+latent[np.logical_not(mask), :] = np.nan
+
+days_delta1 = (datetime.datetime(2022, 1, 1, 0, 0) - datetime.datetime(2019, 1, 1, 0, 0)).days
+days_delta2 = (datetime.datetime(2022, 9, 30, 0, 0) - datetime.datetime(2022, 1, 1, 0, 0)).days
+# sensible_2019 = np.load(files_path_prefix + f'SENSIBLE_2019-2022.npy')
+latent_2019 = np.load(files_path_prefix + f'LATENT_2019-2022.npy')
+
+# full_sensible = np.zeros((29141, days_delta1*4 + (days_delta2+1)*4))
+# full_sensible[:, 0:days_delta1*4] = sensible_2019[:, 0:days_delta1*4]
+# full_sensible[:, days_delta1*4:] = sensible
+# np.save(files_path_prefix + f'SENSIBLE_2019-2022[Oct].npy', full_sensible)
+# np.save(files_path_prefix + f'SENSIBLE_{data_postfix}.npy', sensible)
+
+full_latent = np.zeros((29141, days_delta1*4 + (days_delta2+1)*4))
+full_latent[:, 0:days_delta1*4] = latent_2019[:, 0:days_delta1*4]
+full_latent[:, days_delta1*4:] = latent
+np.save(files_path_prefix + f'LATENT_2019-2022[Oct].npy', full_latent)
+np.save(files_path_prefix + f'LATENT_{data_postfix}.npy', latent)
 raise ValueError
 
 # # latent
