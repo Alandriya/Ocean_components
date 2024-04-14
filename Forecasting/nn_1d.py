46906d45c06816110b019c7fefd4c43ed92aae52
Alandriya <ashcherbinina@mail.ru>
2024-04-14 17:32:05 +0300

    nn v1.0

--- Forecasting/nn_1d.py
+++ Forecasting/nn_1d.py
@@ -1,9 +1,14 @@
+import datetime
+import gc
+import os
+import darts
 import matplotlib.pyplot as plt
 import numpy as np
 from struct import unpack
 
 import pandas as pd
 import sklearn.metrics
+import tqdm
 from sklearn.cluster import KMeans
 from darts.dataprocessing.transformers import Scaler
 from sklearn.metrics import mean_squared_error
@@ -13,6 +18,10 @@ from sklearn.preprocessing import MinMaxScaler
 from darts.models import TransformerModel, ExponentialSmoothing
 from darts.metrics import mape
 from darts.utils.statistics import check_seasonality, plot_acf
+from darts.models.forecasting.torch_forecasting_model import PastCovariatesTorchModel
+from copy import copy
+from plotter import plot_predictions
+import joblib
 
 import warnings
 warnings.filterwarnings("ignore")
@@ -82,7 +91,8 @@ def prepare_clusters(files_path_prefix: str,
                     masked_row_idxes: np.ndarray,
                    clusters: np.ndarray,
                      n_lags: int,
-                     n_forecast: int,):
+                     n_forecast: int,
+                     start_day = datetime.datetime(2019, 1, 1)):
     labels = np.unique(clusters)
     for i in range(len(labels)):
     # for i in range(1):
@@ -94,55 +104,115 @@ def prepare_clusters(files_path_prefix: str,
         train_len = int(array.shape[1] * 2 / 3.0)
         test_len = array.shape[1] - train_len - n_lags * 2 - n_forecast
 
-        x_train = np.zeros((train_len, amount, n_lags))
-        y_train = np.zeros((train_len, amount, n_forecast))
-        x_test = np.zeros((test_len, amount, n_lags))
-        y_test = np.zeros((test_len, amount, n_forecast))
+        df_x_train = pd.DataFrame(columns=['day'] + [f'component_{i}-day_lag_{t}' for i in range(amount)
+                                                         for t in range(1, n_lags + 1)])
+        df_y_train = pd.DataFrame(columns=['day'] + [f'component_{i}-day_{t}' for i in range(amount)
+                                                         for t in range(1, n_forecast + 1)])
 
-        for k in range(amount):
-            row = masked_row_idxes[row_idxes[0][k]]
-            cluster_part[k] = array[row, :]
+        df_x_test = pd.DataFrame(columns=['day'] + [f'component_{i}-day_lag_{t}' for i in range(amount)
+                                                     for t in range(1, n_lags + 1)])
+        df_y_test = pd.DataFrame(columns=['day'] + [f'component_{i}-day_{t}' for i in range(amount)
+                                                     for t in range(1, n_forecast + 1)])
 
-            for t in range(0, train_len):
-                x_train[t, k, :] = array[row, t:t + n_lags].reshape(n_lags)
-                y_train[t, k, :] = array[row, t + n_lags + 1:t + n_lags + 1 + n_forecast].reshape(n_forecast)
+        # x_train = np.zeros((train_len, amount, n_lags))
+        # y_train = np.zeros((train_len, amount, n_forecast))
+        # x_test = np.zeros((test_len, amount, n_lags))
+        # y_test = np.zeros((test_len, amount, n_forecast))
+        #
+        # for k in range(amount):
+        #     row = masked_row_idxes[row_idxes[0][k]]
+        #     cluster_part[k] = array[row, :]
+        #
+        #     for t in range(0, train_len):
+        #         x_train[t, k, :] = array[row, t:t + n_lags].reshape(n_lags)
+        #         y_train[t, k, :] = array[row, t + n_lags + 1:t + n_lags + 1 + n_forecast].reshape(n_forecast)
+        #
+        #     for t in range(0, test_len):
+        #         x_test[t, k, :] = array[row, train_len + n_lags + t:train_len + n_lags + t + n_lags].reshape(n_lags)
+        #         y_test[t, k, :] = array[row, train_len + n_lags * 2 + t + 1:train_len + n_lags * 2 + t + 1 + n_forecast].\
+        #             reshape(n_forecast)
+        # np.save(files_path_prefix + f'Forecast/Train/x_train_{i}.npy', x_train)
+        # np.save(files_path_prefix + f'Forecast/Train/y_train_{i}.npy', y_train)
+        # np.save(files_path_prefix + f'Forecast/Test/x_test_{i}.npy', x_test)
+        # np.save(files_path_prefix + f'Forecast/Test/y_test_{i}.npy', y_test)
 
-            for t in range(0, test_len):
-                x_test[t, k, :] = array[row, train_len + n_lags + t:train_len + n_lags + t + n_lags].reshape(n_lags)
-                y_test[t, k, :] = array[row, train_len + n_lags * 2 + t + 1:train_len + n_lags * 2 + t + 1 + n_forecast].\
-                    reshape(n_forecast)
-        np.save(files_path_prefix + f'Forecast/Train/x_train_{i}.npy', x_train)
-        np.save(files_path_prefix + f'Forecast/Train/y_train_{i}.npy', y_train)
-        np.save(files_path_prefix + f'Forecast/Test/x_test_{i}.npy', x_test)
-        np.save(files_path_prefix + f'Forecast/Test/y_test_{i}.npy', y_test)
+        x_train = np.load(files_path_prefix + f'Forecast/Train/x_train_{i}.npy')
+        y_train = np.load(files_path_prefix + f'Forecast/Train/y_train_{i}.npy')
+        x_test = np.load(files_path_prefix + f'Forecast/Test/x_test_{i}.npy')
+        y_test = np.load(files_path_prefix + f'Forecast/Test/y_test_{i}.npy')
+
+        for t_first in range(0, train_len):
+            day = start_day + datetime.timedelta(days=t_first)
+            x_train_list, y_train_list = [day], [day]
+            for comp_idx in range(x_train.shape[1]):
+                for t in range(n_lags):
+                    x_train_list.append(x_train[t_first, comp_idx, t])
+                for t in range(n_forecast):
+                    y_train_list.append(y_train[t_first, comp_idx, t])
+
+            df_x_train.loc[len(df_x_train)] = x_train_list
+            df_y_train.loc[len(df_y_train)] = y_train_list
+
+        print(df_x_train.head(1))
+
+        df_x_train.to_excel(files_path_prefix + f'Forecast/Train/df_x_train_{i}.xlsx', index=False)
+        df_y_train.to_excel(files_path_prefix + f'Forecast/Train/df_y_train_{i}.xlsx', index=False)
+
+        for t_first in range(train_len, train_len + x_test.shape[0]):
+            day = start_day + datetime.timedelta(days=t_first)
+            x_test_list, y_test_list = [day], [day]
+            for comp_idx in range(x_test.shape[1]):
+                for t in range(n_lags):
+                    x_test_list.append(x_test[t_first - train_len, comp_idx, t])
+                for t in range(n_forecast):
+                    y_test_list.append(y_test[t_first - train_len, comp_idx, t])
+
+            df_x_test.loc[len(df_x_test)] = x_test_list
+            df_y_test.loc[len(df_y_test)] = y_test_list
+
+        df_x_test.to_excel(files_path_prefix + f'Forecast/Test/df_x_test_{i}.xlsx', index=False)
+        df_y_test.to_excel(files_path_prefix + f'Forecast/Test/df_y_test_{i}.xlsx', index=False)
     return
 
 
 def train_cluster(files_path_prefix: str,
-                  x_train: np.ndarray,
-                  y_train: np.ndarray,
+                  # x_train: TimeSeries,
+                  targets: list,
                   cluster_idx: int,
+                  n_lags: int,
+                  n_forecast: int,
                   ):
     model = TransformerModel(
-        input_chunk_length=12,
-        output_chunk_length=1,
+        input_chunk_length=n_lags,
+        output_chunk_length=n_forecast,
         batch_size=32,
-        n_epochs=200,
+        n_epochs=25,
         model_name=f"cluster_{cluster_idx}",
-        nr_epochs_val_period=10,
         d_model=16,
         nhead=8,
         num_encoder_layers=2,
         num_decoder_layers=2,
         dim_feedforward=128,
+        # d_model=8,
+        # nhead=4,
+        # num_encoder_layers=1,
+        # num_decoder_layers=1,
+        # dim_feedforward=32,
         dropout=0.1,
         activation="relu",
         random_state=42,
         save_checkpoints=True,
         force_reset=True,
+        work_dir=files_path_prefix + f'Forecast/Models/Clusters/'
     )
-    model.fit(series=(x_train, y_train), verbose=True)
+    model.fit(targets, verbose=True)
     return model
+
+
+def scale(array):
+    arr_min = np.nanmin(array)
+    arr_max = np.nanmax(array)
+    return arr_min, arr_max, (array - arr_min)/(arr_max - arr_min)
 
 
 if __name__ == '__main__':
@@ -150,7 +220,7 @@ if __name__ == '__main__':
     files_path_prefix = 'E:/Nastya/Data/OceanFull/'
     batch_size = 32
     days_known = 14
-    days_prediction = 14
+    days_prediction = 10
     n_clusters = 100
     # --------------------------------------------------------------------------------
     # Mask
@@ -159,39 +229,87 @@ if __name__ == '__main__':
     maskfile.close()
     mask = unpack('?' * 29141, binary_values)
     mask = np.array(mask, dtype=int)
-    masked_rows = np.load(files_path_prefix + f'Forecast/Models/Clusters/rows_idxes.npy')
-    masked_rows.dtype = int
     # ---------------------------------------------------------------------------------------
     # load data
-    # array = np.load(files_path_prefix + f'Fluxes/FLUX_2019-2023_grouped.npy')
-    # filename = 'flux'
+    array = np.load(files_path_prefix + f'Fluxes/FLUX_2019-2023_grouped.npy')
+    # print(array.shape)
+    train_len = int(array.shape[1] * 2 / 3 + 100)
+    train_days = [datetime.datetime(2019, 1, 1) + datetime.timedelta(days=t) for t in range(train_len)]
+    test_days = [datetime.datetime(2019, 1, 1) + datetime.timedelta(days=t) for t in range(train_len, array.shape[1])]
+
+    filename = 'flux'
     # SST_array = np.load(files_path_prefix + f'SST/SST_2019-2023_grouped.npy')
     # press_array = np.load(files_path_prefix + f'Pressure/PRESS_2019-2023_grouped.npy')
     # print(flux_array.shape)
 
-    # clusterize(files_path_prefix, array, mask, n_clusters, filename, 100)
-    # clusters = np.load(files_path_prefix + f'Forecast/Models/Clusters/clusters_{filename}.npy')
+    clusterize(files_path_prefix, array, mask, n_clusters, filename, 100)
+    clusters = np.load(files_path_prefix + f'Forecast/Models/Clusters/clusters_{filename}.npy')
+    rows_idxes = np.load(files_path_prefix + f'Forecast/Models/Clusters/rows_idxes.npy')
+    # print(clusters.shape)
+    # print(clusters)
     # prepare_clusters(files_path_prefix, array, masked_rows, clusters, days_known, days_prediction)
-    df_metrics = pd.DataFrame(columns=['Cluster', 'RMSE'])
+    df_metrics = pd.DataFrame(columns=['cluster', 'RMSE'])
+    df_scaling = pd.DataFrame(columns=['row', 'min', 'max'])
 
-    for i in range(1):
-        x_train = TimeSeries.from_values(np.load(files_path_prefix + f'Forecast/Train/x_train_{i}.npy'))
-        y_train = TimeSeries.from_values(np.load(files_path_prefix + f'Forecast/Train/y_train_{i}.npy'))
+    real_values = np.zeros((161 * 181, days_prediction))
+    real_values[np.logical_not(mask), :] = np.nan
+    predictions = np.zeros((161 * 181, days_prediction))
+    predictions[np.logical_not(mask), :] = np.nan
 
-        x_scaler = Scaler(MinMaxScaler(feature_range=(0, 1)))
-        x_train_scaled = x_scaler.fit_transform(x_train)
-        y_scaler = Scaler(MinMaxScaler(feature_range=(0, 1)))
-        y_train_scaled = y_scaler.fit_transform(y_train)
+    error = 0
+    model_str = 'TFT-complex-simple-clusters100'
+    if not os.path.exists(files_path_prefix + f'Forecast/Models/{model_str}'):
+        os.mkdir(files_path_prefix + f'Forecast/Models/{model_str}')
+        os.mkdir(files_path_prefix + f'Forecast/Models/{model_str}/Clusters')
+    for cluster_idx in range(n_clusters):
+        # if True:
+        #     continue
+    # for cluster_idx in range(1):
+        idxes = np.where(clusters == cluster_idx)[0]
+        targets = list()
+        scale_list = list()
+        for i in range(len(idxes)):
+            row = rows_idxes[idxes[i]]
+            scaled = copy(array[row, :train_len])
+            arr_min = np.min(scaled)
+            arr_max = np.max(scaled)
+            scale_list.append((arr_min, arr_max))
+            df_scaling.loc[len(df_scaling)] = [row, arr_min, arr_max]
+            scaled = (scaled - arr_min) / (arr_max - arr_min)
 
-        model = train_cluster(files_path_prefix, x_train_scaled, y_train_scaled, i)
-        model.save(files_path_prefix + f'Forecast/Models/Clusters/model_{i}.pth')
+            target = pd.Series(scaled, train_days)
+            targets.append(TimeSeries.from_series(target))
+
+        model = train_cluster(files_path_prefix, targets, cluster_idx, days_known, days_prediction)
+        model.save(files_path_prefix + f'Forecast/Models/{model_str}/Clusters/model_{cluster_idx}.pth')
 
         # test
-        x_test = TimeSeries.from_values(np.load(files_path_prefix + f'Forecast/Test/x_test_{i}.npy'))
-        y_test = TimeSeries.from_values(np.load(files_path_prefix + f'Forecast/Test/y_test_{i}.npy'))
-        x_test_scaled = x_scaler.transform(x_test)
-        y_test_scaled = y_scaler.transform(y_test)
+        model = PastCovariatesTorchModel.load(files_path_prefix + f'Forecast/Models/{model_str}/Clusters/model_{cluster_idx}.pth')
+        y_predict_list = model.predict(days_prediction, targets)
 
-        y_predict = model.predict(x_test_scaled)
-        df_metrics.loc[len(df_metrics)] = [i, rmse(y_predict, y_test_scaled)]
-        df_metrics.to_excel(files_path_prefix + f'Forecast/df_metrics.xlsx', index=False)
+        cluster_error = 0
+        for i in range(len(idxes)):
+            row = rows_idxes[idxes[i]]
+            y_test = array[row, 0:days_prediction]
+            y_predict = y_predict_list[i].values()
+            y_predict = np.array(y_predict).flatten()
+            # y_predict = np.zeros_like(y_test)
+
+            y_predict = y_predict * (scale_list[i][1] - scale_list[i][0]) + scale_list[i][0]
+            real_values[row, :] = y_test
+            predictions[row, :] = y_predict
+            cluster_error += np.sum(np.square(y_predict - y_test))
+
+        print(f'Cluster {cluster_idx} RMSE = {np.sqrt(cluster_error):.1f}')
+        error += cluster_error
+
+    # print(f'Sum RMSE = {np.sqrt(error): .1f}')
+    # df_scaling.to_excel(files_path_prefix + f'Forecast/Models/{model_str}/df_scaling.xlsx', index=False)
+    # np.save(files_path_prefix + f'Forecast/Models/{model_str}/predictions.npy', predictions)
+    # np.save(files_path_prefix + f'Forecast/real.npy', real_values)
+
+    # plot predictions
+    real_values = np.load(files_path_prefix + f'Forecast/real.npy')
+    predictions = np.load(files_path_prefix + f'Forecast/Models/{model_str}/predictions.npy')
+    plot_predictions(files_path_prefix, real_values.reshape((161, 181, -1)), predictions.reshape((161, 181, -1)),
+                     model_str, test_days[0])
--- Forecasting/plotter.py
+++ Forecasting/plotter.py
@@ -1,66 +1,123 @@
+import datetime
+from sklearn.metrics import mean_squared_error
 import matplotlib.pyplot as plt
 import numpy as np
 import tqdm
-
 from mpl_toolkits.axes_grid1 import make_axes_locatable
-
+import matplotlib.colors as colors
 files_path_prefix = 'D://Data/OceanFull/'
 
+
+def hex_to_rgb(value):
+    """
+    Converts hex to rgb colours
+    value: string of 6 characters representing a hex colour.
+    Returns: list length 3 of RGB values
+    """
+    value = value.strip("#")  # removes hash symbol if present
+    lv = len(value)
+    return tuple(int(value[i:i + lv // 3], 16) for i in range(0, lv, lv // 3))
+
+
+def rgb_to_dec(value):
+    """
+    Converts rgb to decimal colours (i.e. divides each value by 256)
+    value: list (length 3) of RGB values
+    Returns: list (length 3) of decimal values
+    """
+    return [v / 256 for v in value]
+
+
+def get_continuous_cmap(hex_list, float_list=None):
+    """
+    creates and returns a color map that can be used in heat map figures.
+    If float_list is not provided, colour map graduates linearly between each color in hex_list.
+    If float_list is provided, each color in hex_list is mapped to the respective location in float_list.
+
+    Parameters
+    ----------
+    hex_list: list of hex code strings
+    float_list: list of floats between 0 and 1, same length as hex_list. Must start with 0 and end with 1.
+
+    Returns
+    ----------
+    colour map
+    """
+    rgb_list = [rgb_to_dec(hex_to_rgb(i)) for i in hex_list]
+    if float_list:
+        pass
+    else:
+        float_list = list(np.linspace(0, 1, len(rgb_list)))
+
+    cdict = dict()
+    for num, col in enumerate(['red', 'green', 'blue']):
+        col_list = [[float_list[i], rgb_list[i][num], rgb_list[i][num]] for i in range(len(float_list))]
+        cdict[col] = col_list
+    cmp = colors.LinearSegmentedColormap('my_cmp', segmentdata=cdict, N=256)
+    return cmp
 
 def plot_predictions(files_path_prefix: str,
                      Y_test: np.ndarray,
                      Y_predict: np.ndarray,
-                     model_name: str):
-    fig, axs = plt.subplots(1, 3, figsize=(20, 10))
+                     model_name: str,
+                     start_day: datetime.datetime):
+    print('Plotting')
+    days_prediction = Y_predict.shape[2]
+    fig, axs = plt.subplots(3, days_prediction, figsize=(5*days_prediction, 15))
 
-    axs[0].set_title('Real values', fontsize=20)
-    axs[1].set_title('Predicted values', fontsize=20)
-    axs[2].set_title('Absolute difference', fontsize=20)
+    # axs[0].set_title('Real values', fontsize=20)
+    # axs[1].set_title('Predicted values', fontsize=20)
+    # axs[2].set_title('Absolute difference', fontsize=20)
 
-    img = [None for _ in range(3)]
-    cax = [None for _ in range(3)]
+    img = [[None for _ in range(days_prediction)] for _ in range(3)]
+    cax = [[None for _ in range(days_prediction)] for _ in range(3)]
 
     y_min = min(np.nanmin(Y_test), np.nanmin(Y_predict))
     y_max = max(np.nanmax(Y_test), np.nanmax(Y_predict))
-    # cmap = get_continuous_cmap(['#000080', '#ffffff', '#ff0000'], [0, (1.0 - y_min) / (y_max - y_min), 1])
-    cmap = plt.get_cmap('Blues')
+    cmap = get_continuous_cmap(['#000080', '#ffffff', '#ff0000'], [0, (1.0 - y_min) / (y_max - y_min), 1])
+    # cmap = plt.get_cmap('Blues').copy()
     cmap.set_bad('darkgreen', 1.0)
-    for t in tqdm.tqdm(range(Y_test.shape[0])):
-        difference = np.array(np.abs(Y_predict[t] - Y_test[t]))
-        rmse = np.sqrt(np.sum(np.square(difference)))
-        fig.suptitle(f'{model_name}, day {t}, RMSE = {rmse:.2f}', fontsize=30)
+
+    cmap_diff = plt.get_cmap('Reds').copy()
+    cmap_diff.set_bad('darkgreen', 1.0)
+
+    mse = 0
+    for t in tqdm.tqdm(range(days_prediction)):
+        difference = np.array(np.abs(Y_predict[:, :, t] - Y_test[:, :, t]))
+        mse += np.sum(np.square(np.nan_to_num(difference)))
+
+        day_str = (start_day + datetime.timedelta(days=t)).strftime('%d.%m.%Y')
+        for i in range(3):
+            divider = make_axes_locatable(axs[i][t])
+            cax[i][t] = divider.append_axes('right', size='5%', pad=0.3)
+
+            axs[0][t].set_title(f'{day_str}, real values', fontsize=16)
+            img[0][t] = axs[0][t].imshow(Y_test[:, :, t],
+                                         interpolation='none',
+                                         cmap=cmap,
+                                         vmin=y_min,
+                                         vmax=y_max)
+
+            axs[1][t].set_title(f'{day_str}, predictions', fontsize=16)
+            img[1][t] = axs[1][t].imshow(Y_predict[:, :, t],
+                                         interpolation='none',
+                                         cmap=cmap,
+                                         vmin=y_min,
+                                         vmax=y_max)
+
+            axs[2][t].set_title(f'{day_str}, absolute difference', fontsize=16)
+            img[2][t] = axs[2][t].imshow(difference,
+                                         interpolation='none',
+                                         cmap=cmap_diff,
+                                         vmin=0,
+                                         vmax=np.nanmax(difference))
 
         for i in range(3):
-            divider = make_axes_locatable(axs[i])
-            cax[i] = divider.append_axes('right', size='5%', pad=0.3)
-        if img[0] is None:
-            img[0] = axs[0].imshow(Y_test[t],
-                                   interpolation='none',
-                                   cmap=cmap,
-                                   vmin=y_min,
-                                   vmax=y_max)
+            fig.colorbar(img[i][t], cax=cax[i][t], orientation='vertical')
 
-            img[1] = axs[1].imshow(Y_predict[t],
-                                   interpolation='none',
-                                   cmap=cmap,
-                                   vmin=y_min,
-                                   vmax=y_max)
-
-            img[2] = axs[2].imshow(difference,
-                                   interpolation='none',
-                                   cmap=plt.get_cmap('Reds'),
-                                   vmin=0,
-                                   vmax=np.nanmax(difference))
-        else:
-            img[0].set_data(Y_test[t])
-            img[1].set_data(Y_predict[t])
-            img[2].set_data(difference)
-
-        for i in range(3):
-            fig.colorbar(img[i], cax=cax[i], orientation='vertical')
-
-        plt.tight_layout()
-        fig.savefig(files_path_prefix + f'videos/Forecast/{model_name}_{t}.png')
+    fig.suptitle(f'{model_name}, RMSE = {np.sqrt(mse):.1f}', fontsize=30)
+    plt.tight_layout()
+    fig.savefig(files_path_prefix + f'videos/Forecast/{model_name}.png')
     return
 
 
